a71f04addc4a378609af7b91a66170720b97f012
Scott Hedrick <me@scottshedrick.com>
2018-02-07 12:25:05 -0600

    added exdoc dependency and continued updating game.ex for more logic

--- islands_engine/lib/islands_engine/game.ex
+++ islands_engine/lib/islands_engine/game.ex
@@ -1,6 +1,8 @@
 defmodule IslandsEngine.Game do
   use GenServer
-  alias IslandsEngine.{Board, Guesses, Rules}
+  alias IslandsEngine.{Board, Coordinate, Guesses, Island, Rules}
+
+  @players [:player1, :player2]
 
   def start_link(name) when is_binary(name), do:
     GenServer.start_link(__MODULE__, name, [])
@@ -26,10 +28,43 @@ defmodule IslandsEngine.Game do
     end
   end
 
+  def handle_call({:position_island, player, key, row, col}, _from, state_data)
+  do
+    board = player_board(state_data, player)
+    with  {:ok, rules} <-
+            Rules.check(state_data.rules, {:position_islands, player}),
+          {:ok, coordinate} <-
+            Coordinate.new(row, col),
+          {:ok, island} <-
+            Island.new(key, coordinate),
+          %{} = board <-
+            Board.position_island(board, key, island)
+    do
+      state_data
+      |> update_board(player, board)
+      |> update_rules(rules)
+      |> reply_success(:ok)
+    else
+      :error -> {:reply, :error, state_data}
+      {:error, :invalid_coordinate} ->
+        {:reply, {:error, :invalid_coordinate}, state_data}
+      {:error, :invalid_island_type} ->
+        {:reply, {:error, :invalid_island_type}, state_data}
+    end
+  end
+
   defp update_player2_name(state_data, name), do:
     put_in(state_data.player2.name, name)
   defp update_rules(state_data, rules), do:
     %{state_data | rules: rules}
+  defp update_board(state_data, player, board), do:
+    Map.update!(state_data, player, fn player -> %{player | board: board} end)
   defp reply_success(state_data, reply), do:
     {:reply, reply, state_data}
+
+  def position_island(game, player, key, row, col) when player in @players, do:
+    GenServer.call(game, {:position_island, player, key, row, col})
+  
+  defp player_board(state_data, player), do: Map.get(state_data, player).board
+
 end
